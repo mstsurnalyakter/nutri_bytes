6dd848926b12cb88e20cafcb897d3d38fdeefbfd
mstsurnalyakter <surnalyms@gmail.com>
2024-03-15 23:13:07 +0600

    banner components completed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import banner from '../assets/banner.png'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
+    <>
+      {/* <!-- banner section --> */}
+      <div
+        className={`hero min-h-[600px] rounded-3xl w-full bg-center  bg-cover  object-cover`}
+        style={{ backgroundImage: `url(${banner})` }}
+      >
+        <div className=""></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className=" w-3/5">
+            <h1 className="mb-5 text-5xl font-bold text-white leading-[76px]">
+              Discover an exceptional cooking class tailored for you!
+            </h1>
+            <p className="mb-5 text-white leading-[30px]">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <div className=" space-x-4">
+              <button className="btn bg-[#0BE58A] hover:bg-[#03d47d] text-[#150B2B] text-xl font-semibold rounded-full">
+                Explore Now
+              </button>
+              <button className="btn btn-outline text-xl font-semibold rounded-full text-white hover:border-white hover:bg-transparent">
+                Our Feedback
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Banner
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className="navbar bg-base-100">
         <div className="navbar-start">
           <div className="dropdown">
-            <div tabindex="0" role="button" className="btn btn-ghost lg:hidden">
+            <div tabIndex="0" role="button" className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -16,15 +16,15 @@ const Navbar = () => {
                 stroke="currentColor"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M4 6h16M4 12h8m-8 6h16"
                 />
               </svg>
             </div>
             <ul
-              tabindex="0"
+              tabIndex="0"
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-[#150B2BB3]"
             >
               <li className="">
@@ -68,9 +68,9 @@ const Navbar = () => {
               className="w-4 h-4 opacity-70"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
             <input type="text" className="grow" placeholder="Search" />
