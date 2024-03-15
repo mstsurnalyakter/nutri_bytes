eaeeb87c39f2111d16371d46d7dd6325c1b58a44
mstsurnalyakter <surnalyms@gmail.com>
2024-03-15 23:25:24 +0600

    Updated banner component text

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,15 +12,14 @@ const Banner = () => {
         <div className=""></div>
         <div className="hero-content text-center text-neutral-content">
           <div className=" w-3/5">
-            <h1 className="mb-5 text-5xl font-bold text-white leading-[76px]">
+            <h1 className="mb-5 text-4xl font-bold text-white leading-[45px] lg:leading-[55px]">
               Discover an exceptional cooking class tailored for you!
             </h1>
             <p className="mb-5 text-white leading-[30px]">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              Embark on a culinary journey tailored to your taste buds with
+              NutriBytes exceptional cooking classes!
             </p>
-            <div className=" space-x-4">
+            <div className="flex items-center justify-center gap-3">
               <button className="btn bg-[#0BE58A] hover:bg-[#03d47d] text-[#150B2B] text-xl font-semibold rounded-full">
                 Explore Now
               </button>
